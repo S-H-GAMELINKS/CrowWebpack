76a728486ac78c37d2e29c80112c50d6ca127dc2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-20 22:08:21 +0900

    using vue components

--- CrowWebpack/assets/webpack.config.js
+++ CrowWebpack/assets/webpack.config.js
@@ -1,7 +1,20 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'index.js',
         path: `${__dirname}`
-    }
-}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}   
